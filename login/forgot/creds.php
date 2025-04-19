f8c38280188dc2bfab5b7cb0a7a9fc0755619752
Zackary Hayworth <zackary.hayworth.245@my.csun.edu>
2025-04-19 14:29:50 -0700

    Create creds.php

---
+++ login/forgot/creds.php
@@ -0,0 +1,14 @@
+<?php
+/**#@+
+ * Email settings for PHPMailer
+ */
+if (!defined('EMAIL_FROM_ADDRESS')) {
+	define('EMAIL_FROM_ADDRESS', 'grocere481@gmail.com'); // your Grocer-E email
+}
+if (!defined('EMAIL_FROM_NAME')) {
+	define('EMAIL_FROM_NAME', 'Grocer-E');
+}
+if (!defined('EMAIL_APP_PASSWORD')) {
+	define('EMAIL_APP_PASSWORD', 'llzo stkf sazp xlco'); // your Gmail app password
+}
+?>
